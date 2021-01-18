e41200891d54109114cf468086bf9e00e88e2d2c
Felipe Barros <felipebarros.dev@gmail.com>
2021-01-18 19:28:20 -0300

    db dinamico

--- src/mongodb/connection.ts
+++ src/mongodb/connection.ts
@@ -4,7 +4,7 @@ import { SettingsModel } from './Models/Settings';
 export const makeConnection = () => {
   mongoose
     .connect(
-      `mongodb+srv://aqu1les:${process.env.DB_PASSWORD}@cluster0-kvfg5.mongodb.net/pamonha?retryWrites=true&w=majority`,
+      `mongodb+srv://aqu1les:${process.env.DB_PASSWORD}@cluster0-kvfg5.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
       {
         //@ts-ignore
         useNewUrlParser: true,
